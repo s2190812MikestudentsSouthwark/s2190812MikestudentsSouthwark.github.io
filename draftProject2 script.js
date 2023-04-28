faf3aedf4fcafa979d4989efb371187c422f439c
s2190812MikestudentsSouthwark <118312760+s2190812MikestudentsSouthwark@users.noreply.github.com>
2023-04-28 12:53:58 +0100

    change questions

--- draftProject2 script.js
+++ draftProject2 script.js
@@ -33,7 +33,9 @@ const questions = [
  
    
   {Type:1,Question: " Which of the following is not true in relation to unilateral contracts ",
-   a:5,b:6,c:7,d:10,correct:[5,6]},
+   a:" Only one party makes a promise " ,b: " Acceptance requires full performance of the contract",
+   c: "The offeror may withdraw the offerat anytime before acceptance takes place"
+        ,d:"The offeree need not communicate their intention to accept the offer" , correct:[b,c]},
  
   {Type:2,Question: 
     "True or false, it is an offence to deface a United Kingdom banknote by writing on it?",
@@ -41,7 +43,11 @@ const questions = [
  
   {Type:3,Question: 
     " Which of the following is not correct in relation to withdrawing an offer",
-   a:8,b:6,c:7,d:9,correct:7},
+   a: "An offeror is free to withdraw at any time before acceptance takes place" ,
+   b:"The offeror may withdraw the offer within a reasonable time after acceptance",
+   ,c: "The offeror may withdraw the offerat anytime before acceptance takes place", 
+   d:" The offeree need not communicate their intention to accept the offer", 
+   correct: b},
  
    
 
